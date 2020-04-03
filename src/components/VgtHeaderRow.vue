b04748e118530d96e1c6870e442afd576252bdd8
cbonaudo.externe <cbonaudo.externe@bedrockstreaming.com>
2020-04-03 09:41:14 +0200

    Recursive check of children checkbox and adjust vgtSelected if needed

--- src/components/VgtHeaderRow.vue
+++ src/components/VgtHeaderRow.vue
@@ -1,116 +1,177 @@
 <template>
-<tr>
-  <th
-    v-if="headerRow.mode === 'span'"
-    class="vgt-left-align vgt-row-header"
-    :colspan="fullColspan"
-    @click="collapsable ? $emit('vgtExpand', !headerRow.vgtIsExpanded) : () => {}">
-    <span v-if="collapsable" class="triangle" :class="{ 'expand': headerRow.vgtIsExpanded }"></span>
-    <slot
-      :row="headerRow"
-      name="table-header-row">
-      <span v-if="headerRow.html" v-html="headerRow.label">
-      </span>
-      <span v-else>
-        {{ headerRow.label }}
-      </span>
-    </slot>
-  </th>
-  <!-- if the mode is not span, we display every column -->
-  <th
-    class="vgt-row-header"
-    v-if="headerRow.mode !== 'span' && lineNumbers"></th>
-  <th
-    class="vgt-row-header"
-    v-if="headerRow.mode !== 'span' && selectable">
-    <span v-if="columnCollapsable(-1)" class="triangle" :class="{ 'expand': headerRow.vgtIsExpanded }"
-      @click="columnCollapsable(-1) ? $emit('vgtExpand', !headerRow.vgtIsExpanded) : () => {}"></span>
-    <input
-      type="checkbox"
-      @click.stop="onCheckboxClicked(headerRow, 0, $event)"
-      :checked="headerRow.vgtSelected"
-    />
-  </th>
-  <th
-    v-if="headerRow.mode !== 'span' && !column.hidden"
-    v-for="(column, i) in columns"
-    :key="i"
-    class="vgt-row-header"
-    :class="getClasses(i, 'td')"
-    @click="columnCollapsable(i) ? $emit('vgtExpand', !headerRow.vgtIsExpanded) : () => {}">
-    <span v-if="columnCollapsable(i)" class="triangle" :class="{ 'expand': headerRow.vgtIsExpanded }"></span>
-    <slot
-      :row="headerRow"
-      :column="column"
-      :formattedRow="formattedRow(headerRow, true)"
-      name="table-header-row">
-      <span v-if="!column.html">
-        {{ collectFormatted(headerRow, column, true) }}
-      </span>
-      <span v-if="column.html" v-html="collectFormatted(headerRow, column, true)">
-      </span>
-    </slot>
-  </th>
-</tr>
+  <tr>
+    <th
+      v-if="headerRow.mode === 'span'"
+      class="vgt-left-align vgt-row-header"
+      :colspan="fullColspan"
+      @click="
+        collapsable ? $emit('vgtExpand', !headerRow.vgtIsExpanded) : () => {}
+      "
+    >
+      <span
+        v-if="collapsable"
+        class="triangle"
+        :class="{ expand: headerRow.vgtIsExpanded }"
+      ></span>
+      <slot :row="headerRow" name="table-header-row">
+        <span v-if="headerRow.html" v-html="headerRow.label"> </span>
+        <span v-else>
+          {{ headerRow.label }}
+        </span>
+      </slot>
+    </th>
+    <!-- if the mode is not span, we display every column -->
+    <th
+      class="vgt-row-header"
+      v-if="headerRow.mode !== 'span' && lineNumbers"
+    ></th>
+    <th class="vgt-row-header" v-if="headerRow.mode !== 'span' && selectable">
+      <span
+        v-if="columnCollapsable(-1)"
+        class="triangle"
+        :class="{ expand: headerRow.vgtIsExpanded }"
+        @click="
+          columnCollapsable(-1)
+            ? $emit('vgtExpand', !headerRow.vgtIsExpanded)
+            : () => {}
+        "
+      ></span>
+      <input
+        type="checkbox"
+        @click.stop="selectRow(headerRow, 0, $event)"
+        :checked="allSelected"
+        :indeterminate.prop="almostAllSelected"
+      />
+    </th>
+    <th
+      v-if="headerRow.mode !== 'span' && !column.hidden"
+      v-for="(column, i) in columns"
+      :key="i"
+      class="vgt-row-header"
+      :class="getClasses(i, 'td')"
+      @click="
+        columnCollapsable(i)
+          ? $emit('vgtExpand', !headerRow.vgtIsExpanded)
+          : () => {}
+      "
+    >
+      <span
+        v-if="columnCollapsable(i)"
+        class="triangle"
+        :class="{ expand: headerRow.vgtIsExpanded }"
+      ></span>
+      <slot
+        :row="headerRow"
+        :column="column"
+        :formattedRow="formattedRow(headerRow, true)"
+        name="table-header-row"
+      >
+        <span v-if="!column.html">
+          {{ collectFormatted(headerRow, column, true) }}
+        </span>
+        <span
+          v-if="column.html"
+          v-html="collectFormatted(headerRow, column, true)"
+        >
+        </span>
+      </slot>
+    </th>
+  </tr>
 </template>
 
 <script>
+import {
+  recursiveHasRowUnselected,
+  recursiveHasRowSelected,
+  recursiveSelect
+} from "./utils/recursive";
+
 export default {
-  name: 'VgtHeaderRow',
+  name: "VgtHeaderRow",
   props: {
     headerRow: {
-      type: Object,
+      type: Object
     },
     columns: {
-      type: Array,
+      type: Array
     },
     lineNumbers: {
-      type: Boolean,
+      type: Boolean
     },
     selectable: {
-      type: Boolean,
+      type: Boolean
     },
     collapsable: {
       type: [Boolean, Number],
       default: false
     },
     collectFormatted: {
-      type: Function,
+      type: Function
     },
     formattedRow: {
-      type: Function,
+      type: Function
     },
     getClasses: {
-      type: Function,
+      type: Function
     },
     fullColspan: {
-      type: Number,
+      type: Number
     },
     onCheckboxClicked: {
-      type: Function,
-    },
+      type: Function
+    }
   },
   data() {
-    return {
-    };
+    return {};
+  },
+  watch: {
+    headerRow: {
+      deep: true,
+      handler() {
+        console.log(this.headerRow.name);
+        this.$set(this.headerRow, "vgtSelected", this.allSelected);
+        console.log(this.headerRow.vgtSelected);
+      }
+    }
   },
   computed: {
+    allSelected() {
+      if (this.headerRow.children) {
+        return !recursiveHasRowUnselected(this.headerRow);
+      } else {
+        return this.headerRow.vgtSelected;
+      }
+    },
+    almostAllSelected() {
+      if (this.headerRow.children && !this.allSelected) {
+        return recursiveHasRowSelected(this.headerRow);
+      } else {
+        return false;
+      }
+    }
   },
   methods: {
-    columnCollapsable: function (currentIndex) {
+    columnCollapsable: function(currentIndex) {
       if (this.collapsable === true) {
         return currentIndex === 0;
       }
       return currentIndex === this.collapsable;
+    },
+    selectRow(row, index, event) {
+      this.onCheckboxClicked(row, index, event);
+      if (this.headerRow.children) {
+        if (this.allSelected) {
+          recursiveSelect(this.headerRow.children, false, this.$set);
+        } else {
+          recursiveSelect(this.headerRow.children, true, this.$set);
+        }
+        this.headerRow.vgtSelected = this.allSelected;
+      }
     }
   },
-  mounted() {
-  },
-  components: {
-  },
+  mounted() {},
+  components: {}
 };
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
